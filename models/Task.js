a34cb4728be6a7a675fce2e1d4b855bf014c1f5f
radonangelika <radonangelika1@gmail.com>
2025-06-11 09:37:54 +0200

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,9 +1,13 @@
- const mongoose = require('mongoose');
-
+// Import biblioteki Mongoose do pracy z MongoDB
+const mongoose = require('mongoose');
+// Tworzenie nowego schematu dla dokumentu Task
 const TaskSchema = new mongoose.Schema({
   text: String,
+  // Pole 'completed' określa, czy zadanie jest ukończone
+  // Domyślnie jest ustawione na 'false'
   completed: { type: Boolean, default: false }
 });
-
+// Eksportujemy model Mongoose o nazwie 'Task'
+// Będzie używany do tworzenia, odczytu, aktualizacji i usuwania zadań w bazie danych
 module.exports = mongoose.model('Task', TaskSchema);
 
